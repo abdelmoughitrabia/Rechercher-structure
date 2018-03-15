d32ee3374e4044bba98068f2ec774e9143137810
abdelmoughitrabia <abdelmoughit.rabia@usmba.ac.ma>
2018-03-15 18:11:28 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RechercherComponent} from './rechercher/rechercher.component';
+import {PanelModule} from 'primeng/panel';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CheckboxModule, DropdownModule, InputTextareaModule, InputTextModule, RadioButtonModule} from 'primeng/primeng';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RechercherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PanelModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    DropdownModule,
+    ButtonModule,
+    CheckboxModule,
+    RadioButtonModule,
+    TableModule,
+    InputTextareaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
